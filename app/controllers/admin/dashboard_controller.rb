82068c3ecddd297a2d49063ac536d4233299288a
Fraz Ahsan <fraz.ahsan17@gmail.com>
2019-10-24 17:42:34 +0500

    Authorization added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,41 +1,45 @@
 class Admin::DashboardController < AdminController
+  # load_and_authorize_resource :mcard
+  
   def index
-    id = Content.where(name: params[:page]).first
-    if params[:page].present? && id.present?
-      views = TrackView.where(content_id: id.id).group_by_hour(:created_at, range: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, time_zone: Time.zone.name, format: "%-l %p").count
-      @today = TrackView.where(content_id: id.id, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-      @tm = @today.map{|x| {lat: x.latitude, lng: x.longitude}}
-      @day = views.keys
-      @daydata = views.values
-      views = TrackView.where(content_id: id.id).group_by_day(:created_at, range: 1.week.ago..Time.zone.now, time_zone: Time.zone.name, format: "%a").count
-      @lastweek = TrackView.where(content_id: id.id, created_at: 1.week.ago..Time.zone.now)
-      @wm = @lastweek.map{|x| {lat: x.latitude, lng: x.longitude}}
-      @week = views.keys
-      @weekdata = views.values
-      views = TrackView.where(content_id: id.id).group_by_day(:created_at, range: 1.month.ago..Time.zone.now, time_zone: Time.zone.name, format: "%d %b").count
-      @lastmonth = TrackView.where(content_id: id.id, created_at: 1.month.ago..Time.zone.now)
-      @mm = @lastmonth.map{|x| {lat: x.latitude, lng: x.longitude}}
-      @month = views.keys
-      @monthdata = views.values
-    else
-      @day = []
-      @daydata = []
-      @week = []
-      @weekdata = []
-      @month = []
-      @monthdata = []
-      @today = []
-      @lastweek = []
-      @lastmonth = []
-      @tm =[]
-      @wm = []
-      @mm = []
-    end  
-
+    @mcard = Mcard.find params[:id]
+    authorize!(:read, @mcard)
+    # id = Content.where(name: params[:page]).first
+    # if params[:page].present? && id.present?
+    #   views = TrackView.where(content_id: id.id).group_by_hour(:created_at, range: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, time_zone: Time.zone.name, format: "%-l %p").count
+    #   @today = TrackView.where(content_id: id.id, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    #   @tm = @today.map{|x| {lat: x.latitude, lng: x.longitude}}
+    #   @day = views.keys
+    #   @daydata = views.values
+    #   views = TrackView.where(content_id: id.id).group_by_day(:created_at, range: 1.week.ago..Time.zone.now, time_zone: Time.zone.name, format: "%a").count
+    #   @lastweek = TrackView.where(content_id: id.id, created_at: 1.week.ago..Time.zone.now)
+    #   @wm = @lastweek.map{|x| {lat: x.latitude, lng: x.longitude}}
+    #   @week = views.keys
+    #   @weekdata = views.values
+    #   views = TrackView.where(content_id: id.id).group_by_day(:created_at, range: 1.month.ago..Time.zone.now, time_zone: Time.zone.name, format: "%d %b").count
+    #   @lastmonth = TrackView.where(content_id: id.id, created_at: 1.month.ago..Time.zone.now)
+    #   @mm = @lastmonth.map{|x| {lat: x.latitude, lng: x.longitude}}
+    #   @month = views.keys
+    #   @monthdata = views.values
+    # else
+    #   @day = []
+    #   @daydata = []
+    #   @week = []
+    #   @weekdata = []
+    #   @month = []
+    #   @monthdata = []
+    #   @today = []
+    #   @lastweek = []
+    #   @lastmonth = []
+    #   @tm =[]
+    #   @wm = []
+    #   @mm = []
+    # end  
+    
   end
 
-  def destroy_data
-    TrackView.destroy_all
-    redirect_to admin_contents_path, alert: "Data has been reset"
-  end
+  # def destroy_data
+  #   TrackView.destroy_all
+  #   redirect_to admin_contents_path, alert: "Data has been reset"
+  # end
 end
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,16 @@ class AdminController < ActionController::Base
     before_action :authenticate_sub_admin!
 
     layout 'admin'
+
+    rescue_from CanCan::AccessDenied do |exception|
+      respond_to do |format|
+        format.json { head :forbidden, content_type: 'text/html' }
+        format.html { redirect_to main_app.root_url, notice: exception.message }
+        format.js   { head :forbidden, content_type: 'text/html' }
+      end
+    end
+
+    def current_ability
+        @current_ability ||= Ability.new(current_sub_admin)
+    end
 end
