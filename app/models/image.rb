5f02a5fa990c3526567731ace0d5a1ebe56b481d
Fraz Ahsan <fraz.ahsan17@gmail.com>
2019-10-28 14:29:24 +0500

    Models commented

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,16 +1,16 @@
 class Image < ApplicationRecord
-	belongs_to :imageable, polymorphic: true
-	has_attached_file :file, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
-	validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
-	has_many :mcard_codes	
+	# belongs_to :imageable, polymorphic: true
+	# has_attached_file :file, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
+	# validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
+	# has_many :mcard_codes	
 
 
-	before_save :set_hashid
+	# before_save :set_hashid
 
-	def set_hashid
-		if self.imageable_type == "Mcard"
-			self.hashid = SecureRandom.urlsafe_base64(10)
-			self.file_file_name = "#{self.hashid}.jpg"
-		end
-	end
+	# def set_hashid
+	# 	if self.imageable_type == "Mcard"
+	# 		self.hashid = SecureRandom.urlsafe_base64(10)
+	# 		self.file_file_name = "#{self.hashid}.jpg"
+	# 	end
+	# end
 end
