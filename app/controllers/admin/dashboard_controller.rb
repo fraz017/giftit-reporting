2691ca33665abaeb32929ae7164d37e748193af2
Fraz Ahsan <fraz.ahsan17@gmail.com>
2019-11-04 21:37:24 +0500

    Google maps added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -26,6 +26,7 @@ class Admin::DashboardController < AdminController
 
       views = @mcard.mcard_codes.group_by_hour(:recieved_at, range: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day, time_zone: Time.zone.name, format: "%-l %p").count
       @today = @mcard.mcard_codes.where(recieved_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+      @tm = @today.map{|x| {lat: x.lat, lng: x.lng} if (x.lat.present? && x.lat != 0)}.compact
       @todayS = @mcard.mcard_codes.where("recieved_at IS ?", nil).where(sent_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
       @day = views.keys
       @daydata = views.values
@@ -33,6 +34,7 @@ class Admin::DashboardController < AdminController
       views = @mcard.mcard_codes.group_by_day(:recieved_at, range: 1.week.ago..Time.zone.now, time_zone: Time.zone.name, format: "%a").count
       
       @lastweek = @mcard.mcard_codes.where("recieved_at IS NOT ?", nil).where(recieved_at: 1.week.ago..Time.zone.now)
+      @wm = @lastweek.map{|x| {lat: x.lat, lng: x.lng} if (x.lat.present? && x.lat != 0)}.compact
       @lastweekS = @mcard.mcard_codes.where("recieved_at IS ?", nil).where(sent_at: 1.week.ago..Time.zone.now)
       @week = views.keys
       @weekdata = views.values
@@ -44,6 +46,7 @@ class Admin::DashboardController < AdminController
 
 
       @lastmonth = @mcard.mcard_codes.where(recieved_at: 1.month.ago..Time.zone.now)
+      @mm = @lastmonth.map{|x| {lat: x.lat, lng: x.lng} if (x.lat.present? && x.lat != 0)}.compact
       @lastmonthS = @mcard.mcard_codes.where("recieved_at IS ?", nil).where(sent_at: 1.month.ago..Time.zone.now)
       @lastSixMonth = @mcard.mcard_codes.where(created_at: 6.month.ago..Time.zone.now).count
       @lastSixMonthR = @mcard.mcard_codes.where("recieved_at IS NOT ?", nil).where(recieved_at: 6.month.ago..Time.zone.now).count
@@ -59,7 +62,10 @@ class Admin::DashboardController < AdminController
 
       views = @mcard.mcard_codes.group_by_month(:created_at, range: 6.month.ago..Time.zone.now, time_zone: Time.zone.name, format: "%b").count
       @lsmlabels = views.keys
-      @lsmdata = views.values      
+      @lsmdata = views.values  
+      
+      
+
   end
 
   # def destroy_data
